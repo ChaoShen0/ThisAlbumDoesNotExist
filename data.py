2c809f9b08ec507f3c946fe8a70bac4323edaa8a
Austin <austindoolittle@gmail.com>
2019-04-13 20:42:43 -0400

    Add print statements, order albums by artist

--- data.py
+++ data.py
@@ -40,42 +40,54 @@ def spotipy_iter(sp, response):
 def scrape_discog(sp, artist_id):
     albums = sp.artist_albums(artist_id)
 
-    discog_dict = {}
+    scraped_albums = []
     for album in spotipy_iter(sp, albums):
         album_id = album['id']
 
         # skip the album if we already have it or it doesn't have images
-        if album_id in discog_dict or not album['images']:
+        if album_id in scraped_albums or not album['images']:
             continue
 
-        # it could be this is just a feature, assuming main artist is listed first
-        artist_name = album['artists'][0]['name'] if album['artists'] else 'NA'
-
-        discog_dict[album_id] = {
+        scraped_albums.append({
             'name': album['name'],
-            'url': album['images'][0]['url'],
-            'artist': artist_name
-        }
+            'url': album['images'][0]['url'],\
+            'id': album_id
+        })
 
-    return discog_dict
+    return scraped_albums
 
-def scrape_playlist(sp, playlist_id, username):
+def scrape_playlist(sp, album_dict, playlist_id, username):
     playlist = sp.user_playlist(username, playlist_id=playlist_id)
     
-    playlist_dict = {}
-    for track in spotipy_iter(sp, playlist['tracks']):
+    track_count = playlist['tracks']['total']
+    for i, track in enumerate(spotipy_iter(sp, playlist['tracks'])):
         for artist in track['track']['artists']:
-            playlist_dict.update(scrape_discog(sp, artist['id']))
-    
-    return playlist_dict
+            artist_name = artist['name']
+            artist_id = artist['id']
+
+            sys.stdout.write("\033[K")
+            print('\t[%i/%i] Artist name: %s\r'%(i+1, track_count, artist_name), end='')
+            
+            if artist_id in album_dict:
+                continue
+
+            album_dict[artist_id] = {
+                'name': artist_name,
+                'albums': scrape_discog(sp, artist['id']),
+            }
+
+    print()
+    return album_dict
 
 def scrape_album_artwork(sp, username):
     user_playlists = sp.user_playlists(username)
     album_dict = {}
-
-    for playlist in spotipy_iter(sp, user_playlists):
-        album_dict.update(scrape_playlist(sp, playlist['id'], username))
+    playlist_count = user_playlists['total']
+    for i, playlist in enumerate(spotipy_iter(sp, user_playlists)):
+        print('[%i/%i] Playlist name: %s'%(i + 1, playlist_count, playlist['name']))
+        album_dict.update(scrape_playlist(sp, album_dict, playlist['id'], username))
     
+    print()
     return album_dict
 
 def write_to_file(album_dict, output_file):
@@ -94,13 +106,22 @@ def download_image(out_dir, album_id, album_image, overwrite=False):
 
     req.urlretrieve(album_image, filepath)
 
-def download_images(album_dict, output_directory, overwrite=False):
+def download_images(artist_dict, output_directory, overwrite=False):
     util.makedirs(output_directory)
-    for album_id, album_info in album_dict.items():
-        try:
-            download_image(output_directory, album_id, album_info['url'], overwrite=overwrite)
-        except (urlerr.HTTPError, urlerr.ContentTooShortError):
-            print('Could not retrieve %s by %s'%(album_info['name'], album_info['artist']))
+    total_album_count = sum(len(a['albums']) for a in artist_dict.values())
+    retrieved_count = 0
+    for artist_info in artist_dict.values():
+        for album in artist_info['albums']:
+            sys.stdout.write("\033[K")
+            print('\t[%i/%i] Album name: %s\r'%(retrieved_count, total_album_count, album['name']), end='')
+
+            try:
+                download_image(output_directory, album['id'], album['url'], overwrite=overwrite)
+            except (urlerr.HTTPError, urlerr.ContentTooShortError):
+                print('Could not retrieve %s by %s'%(album['name'], artist_info['name']))
+            
+            retrieved_count += 1
+            
 
 def download_image_data(username, data_directory, list_file, overwrite=False):
     # authorize
@@ -110,15 +131,21 @@ def download_image_data(username, data_directory, list_file, overwrite=False):
     # create our client
     sp = spotipy.Spotify(token_info)
     
-    if os.path.exists(list_file):
-        album_dict = json.load(list_file)
+    if os.path.exists(list_file) and not overwrite:
+        print('Pulling cached list file from %s'%list_file)
+        with open(list_file, 'r') as fp:
+            album_dict = json.load(fp)
     else:
+        print('Scraping data from Spotify')
+        print('Grab yourself a beer, this is going to take a while\n')
+
         album_dict = scrape_album_artwork(sp, username)
 
         # write our image url list to a file
         write_to_file(album_dict, list_file)
 
     # download all of the images in our image list
+    print('Beginning image download...')
     download_images(album_dict, data_directory, overwrite=overwrite)
 
 
